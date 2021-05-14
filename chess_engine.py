cce891583ffeccd72c1930f405107ac540f3eeb2
ladislaff93 <lado93@gmail.com>
2021-05-14 20:33:55 +0200

    Chess_Engine

---
+++ chess_engine.py
@@ -0,0 +1,91 @@
+class Board():
+    def __init__(self):
+        #first character is b or w (black or white), next character pieces
+        # . R-Rook, P-Pawn
+        #N-knight, B-bishop, Q-queen, K-king.
+        self.board=[
+            [["bR"],["bN"],["bB"],["bQ"],["bK"],["bB"],["bN"],["bR"]],
+            [["bP"],["bP"],["bP"],["bP"],["bP"],["bP"],["bP"],["bP"]],
+            [['--'],['--'],['--'],['--'],['--'],['--'],['--'],['--']],
+            [['--'],['--'],['--'],['--'],['--'],['--'],['--'],['--']],
+            [['--'],['--'],['--'],['--'],['--'],['--'],['--'],['--']],
+            [['--'],['--'],['--'],['--'],['--'],['--'],['--'],['--']],            
+            [["wP"],["wP"],["wP"],["wP"],["wP"],["wP"],["wP"],["wP"]],
+            [["wR"],["wN"],["wB"],["wQ"],["wK"],["wB"],["wN"],["wR"]]
+        ]
+        self.white_first = True                                                                         #logic for white first move. 
+        print('WHITE IS ON THE MOVE!')
+        self.move_log = []                                                                              #move log for algebraic notation    
+  
+    def move_on_board(self, move):
+        self.board[move.start_row][move.start_column]=['--']
+        self.board[move.end_row][move.end_column]=move.piece_moved
+        #self.move_log.append(move.piece_moved)
+        #self.move_log.append(move.piece_captured)
+        move.algebraic_notation(self.move_log)                                                                   
+        self.white_first = not self.white_first
+        print('PIECE '+self.move_log[0]+' MOVE TO '+self.move_log[1])   
+        if not self.white_first:
+            print('BLACK IS ON THE MOVE!')
+        else:
+            print('WHITE IS ON THE MOVE!')    
+        if len(self.move_log) == 2:
+            self.move_log = []    
+        
+    def check_move(self):
+        return self.piece_move()
+
+    def piece_move(self):
+        moves = []
+        for row in range(len(self.board)):
+            for column in range(len(self.board[row])):
+                turn = self.board[row][column][0][0]
+                if turn == 'w' and self.white_first or turn == 'b' and not self.white_first:
+                    piece = self.board[row][column][0][1]
+                    if piece == 'P':
+                       self.pawn_move(row,column,moves) 
+                    elif piece == 'R':
+                        pass
+        return moves
+       
+    def pawn_move(self, row, column, moves):
+       # if self.white_first and row == 6:
+        if self.white_first:
+            if self.board[row-1][column] == ['--']:
+                moves.append(Move_Piece((row,column),(row-1,column),self.board))
+        elif not self.white_first:
+            if self.board[row+1][column] == ['--']:
+                moves.append(Move_Piece((row,column),(row-1,column),self.board))
+
+    def rook_move(self):
+        pass
+
+
+class Move_Piece():
+    def __init__(self, start_sq, end_sq, board):                          #engine part of moving pieces. we take start and end squares from chess_main-->move pieces-->player_clicks list
+        self.start_row = start_sq[0]                                                                    #from chess_main start_sq start row
+        self.start_column = start_sq[1]                                                                 #from chess_main start_sq start column
+        self.end_row = end_sq[0]                                                                        #from chess_main start_sq end row
+        self.end_column = end_sq[1]                                                                     #from chess_main start_sq end column
+        self.piece_moved = board[self.start_row][self.start_column]                                #variable of piece first selected
+        self.piece_captured = board[self.end_row][self.end_column]                                 #variable of piece selected second
+ 
+    def algebraic_notation(self, move_log):
+        self.numbers = ['8','7','6','5','4','3','2','1']                        #numbers for row 
+        self.letters = ['a','b','c','d','e','f','g','h']                        #letters for column
+        self.start_column_letters = self.letters[self.start_column]             
+        self.start_row_numbers = self.numbers[self.start_row]                   
+        self.end_column_letters = self.letters[self.end_column]
+        self.end_row_numbers = self.numbers[self.end_row]
+        move_log.append(self.start_column_letters+self.start_row_numbers)
+        move_log.append(self.end_column_letters+self.end_row_numbers)
+
+
+if __name__ == '__main__':
+    board = Board()
+    print(board.board())
+
+#when white on move and if white pieces are in the way print message 'INVALID MOVE!!' 
+#en passant 
+#pawn changing in base aka Pawn Promotion
+#rosada mala velka aka Castling
---
+++ chess_main.py
@@ -0,0 +1,94 @@
+from chess_engine import Board
+from chess_engine import Move_Piece 
+import pygame
+
+WIDTH = HEIGHT = 510            #setting up the width and height of chess board
+DIMENSIONS = 8                  #number of squares on one side 
+SQ_SIZE = HEIGHT // DIMENSIONS  
+BACKGROUND = (0,0,0)            #colour of the background. probably not needed
+FPS = 60                        #speed of refresing in which we refresh and redraw the game
+BLACK = (125,148,93)            #color for black squares
+WHITE = (238,238,213)           #color for white squares
+IMAGES = dict()                 #dictionary of image pieces
+
+class Window():                                             #setting up window class where all the game redrawing is going to happend. 
+    def __init__(self):
+        self.WIDTH= WIDTH
+        self.HEIGHT= HEIGHT
+        self.BACKGROUND = BACKGROUND
+        self.FPS = FPS
+        self.win = pygame.display.set_mode((WIDTH,HEIGHT))  #dimension of game window
+        pygame.display.set_caption('Chess')                 #title of the window of game
+        self.b = Board()                                    #instentiating the chess logic class from chess_engine.py
+
+    def draw_board(self):
+        for column in range(DIMENSIONS):    #drawing the rectangulars of chess board. iterating over column and row if column and row is 
+            for row in range(DIMENSIONS):   #odd it is light and if even it's dark
+                if ((column+row)%2) == 1: 
+                    self.black_rect = pygame.Rect((row*SQ_SIZE),(column*SQ_SIZE),SQ_SIZE,SQ_SIZE)   #create rect on x,y position of SQ_size
+                    pygame.draw.rect(self.win, BLACK, self.black_rect) 
+                else:
+                    self.white_rect = pygame.Rect((row*SQ_SIZE),(column*SQ_SIZE),SQ_SIZE,SQ_SIZE)   #create rect on x,y position of SQ_size
+                    pygame.draw.rect(self.win, WHITE, self.white_rect) 
+
+    def draw_window(self):
+        self.win.fill(self.BACKGROUND)  #draw a window with background 
+        self.draw_board()               #draw board
+        self.draw_pieces()              #draw pieces on top of the board
+        pygame.display.update()
+        pygame.display.flip()
+
+    def draw_pieces(self):
+        for row in range(DIMENSIONS):
+            for column in range(DIMENSIONS):
+                if (self.b.board[row][column][0]) !='--':
+                    self.piece_rect = self.win.blit(IMAGES[(self.b.board[row][column][0])],pygame.Rect((column*SQ_SIZE), (row*SQ_SIZE),SQ_SIZE,SQ_SIZE))
+
+    def clock_speed(self):
+        self.clock = pygame.time.Clock()
+        self.clock.tick(FPS)
+
+    def pieces_images(self):
+        self.pieces = ['bR','bB','bQ','bK','bN','bP','wR','wB','wQ','wK','wN','wP']
+        for piece in self.pieces:
+            IMAGES[piece] = pygame.transform.scale(pygame.image.load(f'images/{piece}.png'), (SQ_SIZE,SQ_SIZE))
+
+     
+
+
+def main():
+    w = Window()
+    b = w.b
+    w.pieces_images()
+    sq_sel = ()
+    player_clicks = []
+    run = True 
+    while run:
+        w.clock_speed() 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:                  #if a click a left button on mouse the logic gets executed
+                m_x, m_y = pygame.mouse.get_pos()                       #return x and y coordinance of cursor
+                column = m_x//SQ_SIZE                                   #dividing x coordinant of mouse by sq_size to get row on the chess board
+                row = m_y//SQ_SIZE                                      #dividing y coordinant of mouse by sq_size to get column on the chess board
+                if sq_sel == (column,row):   
+                    sq_sel = ()
+                    player_clicks = []
+                else:
+                    sq_sel = (column, row)                              #tuple with the actual square position of mouse(column, row)  
+                    player_clicks.append([sq_sel[1], sq_sel[0]])        #we take the tuple(column, row) and append the piece that is actually on that position
+                
+                if len(player_clicks) == 2:
+                    move = Move_Piece(player_clicks[0],player_clicks[1], b.board)       #instentiation of move_piece class 
+                    if move in b.check_move():
+                        b.move_on_board(move)
+                        sq_sel = ()
+                        player_clicks = []        
+                    else:
+                        print('COCKSUCKAA!!!')
+        w.draw_window()
+    pygame.quit()   
+
+if __name__ == '__main__':
+    main()
