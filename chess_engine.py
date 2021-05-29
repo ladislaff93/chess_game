eb582d103fb154bc96277fc3d2ba6543cd504277
ladislaff93 <lado93@gmail.com>
2021-05-29 11:29:19 +0200

    29.05.2021 checkmate logic added but dont work

--- chess_engine.py
+++ chess_engine.py
@@ -8,56 +8,63 @@ class Board():
             ["bP","bP","bP","bP","bP","bP","bP","bP"],
             ['--','--','--','--','--','--','--','--'],
             ['--','--','--','--','--','--','--','--'],
-            ['--','--','--','wK','--','--','--','--'],
+            ['--','--','--','--','--','--','--','--'],
             ['--','--','--','--','--','--','--','--'],            
             ["wP","wP","wP","wP","wP","wP","wP","wP"],
             ["wR","wN","wB","wQ","wK","wB","wN","wR"]
         ]
         self.white_first = True                                       #logic for white first move. 
-        self.white_king_position = (0,4)
-        self.black_king_position = (7,4)
-        print('WHITE IS ON THE MOVE!')
-        self.move_log = []                                            #move log for algebraic notation    
+        self.black_king_position = (0,4)
+        self.white_king_position = (7,4)
 
-    def undo_move(self,move):
-        self.board[move.start_row][move.end_row] == move.piece_moved
+        print('WHITE IS ON THE MOVE!')
+        self.move_algebraic_notation = []                                            #move log for algebraic notation    
+        self.move_journal = []
+
+    def undo_move(self):
+        if len(self.move_journal) != 0:
+            move = self.move_journal.pop()
+            self.board[move.start_row][move.end_column] == move.pseudolegal_moved
+            self.board[move.end_row][move.end_column] == move.piece_captured
+            self.white_first == False
+            if move.pseudolegal_moved == 'wK':
+                self.white_king_position = (move.start_row, move.start_column) 
+            elif move.pseudolegal_moved == 'bK': 
+                self.black_king_position = (move.start_row, move.start_column) 
 
     def move_on_board(self, move):
         self.board[move.start_row][move.start_column] = '--'              #change the start square after moving piece
-        self.board[move.end_row][move.end_column] = move.piece_moved      #change the end square on start square
-        move.algebraic_notation(self.move_log)                          #return algebraic notation for actual move.                                             
-        if Move_Piece().start_row == self.white_king_position[0] and Move_Piece().start_column == self.white_king_position[1]:
+        self.board[move.end_row][move.end_column] = move.pseudolegal_moved      #change the end square on start square
+        move.algebraic_notation(self.move_algebraic_notation)             #return algebraic notation for actual move.                                             
+        self.move_journal.append(move)
+    
+        if move.pseudolegal_moved == 'wK':
             self.white_king_position = (move.end_row, move.end_column) 
-        elif Move_Piece().start_row == self.black_king_position[0] and Move_Piece().start_column == self.black_king_position[1]: 
+        elif move.pseudolegal_moved == 'bK': 
             self.black_king_position = (move.end_row, move.end_column) 
-            
+
         self.white_first = not self.white_first
-        print('PIECE '+self.move_log[0]+' MOVE TO '+self.move_log[1])   
+
+        print('PIECE '+self.move_algebraic_notation[0]+' MOVE TO '+self.move_algebraic_notation[1])   
         
-        if not self.white_first:
-            print('BLACK IS ON THE MOVE!')
+        print('BLACK IS ON THE MOVE!' if not self.white_first else 'WHITE IS ON THE MOVE!')
 
-        elif self.white_first:
-            print('WHITE IS ON THE MOVE!')    
-
-        if len(self.move_log) == 2:
-            self.move_log = []    
+        if len(self.move_algebraic_notation) == 2:
+            self.move_algebraic_notation = []    
 
     #function for checking if the move what we made is actually legal.    
     #If the current move dont compromise the king piece(capturning).
 
     def legal_move(self):                         
-        pseudolegal_moves = self.piece_move()
-        for i in range(len(pseudolegal_moves)-1, -1, -1):
-            self.move_on_board(pseudolegal_moves[i])
-            self.white_first == False
+        legal_moves = self.pseudolegal_move()
+        for i in range(len(legal_moves)-1, -1, -1):
+            self.move_on_board(legal_moves[i])
+            self.white_first = not self.white_first
             if self.check_king():
-               pseudolegal_moves.remove(pseudolegal_moves[i]) 
+               legal_moves.remove(legal_moves[i]) 
             self.white_first
-        return self.piece_move()   
-
-
-
+            self.undo_move()
+        return legal_moves   
 
     def check_king(self):
         if self.white_first: 
@@ -65,17 +72,19 @@ class Board():
         else: 
             return self.square_attacked(self.black_king_position[0],self.black_king_position[1]) 
 
-
+    '''
+    Method for looking if the square where the kings are can be attacked 
+    '''
     def square_attacked(self, row, column):
-        self.white_first == False
-        opponent_moves = self.piece_move()
-        self.white_first 
+        self.white_first = not self.white_first #switching to the opponent  
+        opponent_moves = self.pseudolegal_move() #generating all possible opponents moves
+        self.white_first    #switching back 
         for move in opponent_moves:
-            if move.end_r == row and move.end_c == column:
+            if move.end_row== row and move.end_column == column:    #checking if square is attacked 
                 return True 
         return False
 
-    def piece_move(self): #for every piece on board function define it possible moves. And whose turn is(b or w)
+    def pseudolegal_move(self): #for every piece on board function define it possible moves. And whose turn is(b or w)
         moves = []
         for row in range(len(self.board)):
             for column in range(len(self.board[row])):
@@ -192,7 +201,7 @@ class Move_Piece():
         self.start_column = start_sq[1]                                                                 #from chess_main start_sq start column
         self.end_row = end_sq[0]                                                                        #from chess_main start_sq end row
         self.end_column = end_sq[1]                                                                     #from chess_main start_sq end column
-        self.piece_moved = board[self.start_row][self.start_column]                                #variable of piece first selected
+        self.pseudolegal_moved = board[self.start_row][self.start_column]                                #variable of piece first selected
         self.piece_captured = board[self.end_row][self.end_column]                                 #variable of piece selected second
         self.ID = self.start_row*1000+self.start_column*100+self.end_row*10+self.end_column
 
@@ -201,10 +210,7 @@ class Move_Piece():
             return self.ID == other.ID
         return False 
 
-    def __repr__(self):
-        return self.piece_captured
-
-    def algebraic_notation(self, move_log):
+    def algebraic_notation(self, move_algebraic_notation):
         #numbers for row
         self.numbers = ['8','7','6','5','4','3','2','1']                         
         #letters for column
@@ -213,8 +219,8 @@ class Move_Piece():
         self.start_row_numbers = self.numbers[self.start_row]                   
         self.end_column_letters = self.letters[self.end_column]
         self.end_row_numbers = self.numbers[self.end_row]
-        move_log.append(self.start_column_letters+self.start_row_numbers)
-        move_log.append(self.end_column_letters+self.end_row_numbers)
+        move_algebraic_notation.append(self.start_column_letters+self.start_row_numbers)
+        move_algebraic_notation.append(self.end_column_letters+self.end_row_numbers)
 
 
 #en passant 
--- chess_main.py
+++ chess_main.py
@@ -1,62 +1,20 @@
+'''
+Importing from modules
+'''
 from chess_engine import Board
 from chess_engine import Move_Piece 
 import pygame
+import gui
 
-WIDTH = HEIGHT = 480            #setting up the width and height of chess board
-DIMENSIONS = 8                  #number of squares on one side 
-SQ_SIZE = HEIGHT // DIMENSIONS  
-BACKGROUND = (0,0,0)            #colour of the background. probably not needed
-FPS = 60                        #speed of refresing in which we refresh and redraw the game
-BLACK = (125,148,93)            #color for black squares
-WHITE = (238,238,213)           #color for white squares
-IMAGES = dict()                 #dictionary of image pieces
+'''
+Instantiating a classes
+'''
+w = gui.Window()
 
-class Window():                                             #setting up window class where all the game redrawing is going to happend. 
-    def __init__(self):
-        self.WIDTH= WIDTH
-        self.HEIGHT= HEIGHT
-        self.BACKGROUND = BACKGROUND
-        self.FPS = FPS
-        self.win = pygame.display.set_mode((WIDTH,HEIGHT))  #dimension of game window
-        pygame.display.set_caption('Chess')                 #title of the window of game
-        self.b = Board()                                    #instentiating the chess logic class from chess_engine.py
-
-    def draw_board(self):
-        for column in range(DIMENSIONS):    #drawing the rectangulars of chess board. iterating over column and row if column and row is 
-            for row in range(DIMENSIONS):   #odd it is light and if even it's dark
-                if ((column+row)%2) == 1: 
-                    self.black_rect = pygame.Rect((row*SQ_SIZE),(column*SQ_SIZE),SQ_SIZE,SQ_SIZE)   #create rect on x,y position of SQ_size
-                    pygame.draw.rect(self.win, BLACK, self.black_rect) 
-                else:
-                    self.white_rect = pygame.Rect((row*SQ_SIZE),(column*SQ_SIZE),SQ_SIZE,SQ_SIZE)   #create rect on x,y position of SQ_size
-                    pygame.draw.rect(self.win, WHITE, self.white_rect) 
-
-    def draw_window(self):
-        self.win.fill(self.BACKGROUND)  #draw a window with background 
-        self.draw_board()               #draw board
-        self.draw_pieces()              #draw pieces on top of the board
-        pygame.display.update()
-        pygame.display.flip()
-
-    def draw_pieces(self):
-        for row in range(DIMENSIONS):
-            for column in range(DIMENSIONS):
-                if (self.b.board[row][column]) !='--':
-                    self.piece_rect = self.win.blit(IMAGES[(self.b.board[row][column])],pygame.Rect((column*SQ_SIZE), (row*SQ_SIZE),SQ_SIZE,SQ_SIZE))
-
-    def clock_speed(self):
-        self.clock = pygame.time.Clock()
-        self.clock.tick(FPS)
-
-    def pieces_images(self):
-        self.pieces = ['bR','bB','bQ','bK','bN','bP','wR','wB','wQ','wK','wN','wP']
-        for piece in self.pieces:
-            IMAGES[piece] = pygame.transform.scale(pygame.image.load(f'images/{piece}.png'), (SQ_SIZE,SQ_SIZE))
-
-
+'''
+Defining main function where game is running
+'''
 def main():
-    w = Window()
-    b = w.b
     w.pieces_images()
     #legal_move return as list of legal moves.   
     sq_sel = ()
@@ -64,20 +22,20 @@ def main():
     #running game while loop
     run = True 
     while run:
-        valid_move = b.legal_move()
+        valid_move = gui.board.legal_move()
         move_check = False
         w.clock_speed() 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            #if a click a left button on mouse the logic gets executed
+            #if a click a left.board.tton on mouse the logic gets executed
             elif event.type == pygame.MOUSEBUTTONDOWN:                  
             #return x and y coordinance of cursor
                 m_x, m_y = pygame.mouse.get_pos()                       
-            #dividing x coordinant of mouse by sq_size to get row on the chess board
-                column = m_x//SQ_SIZE                                   
-            #dividing y coordinant of mouse by sq_size to get column on the chess board
-                row = m_y//SQ_SIZE                                     
+            #dividing x coordinant of mouse.board. sq_size to get row on the chess.board.ard
+                column = m_x//gui.SQ_SIZE                                   
+            #dividing y coordinant of mouse.board. sq_size to get column on the chess.board.ard
+                row = m_y//gui.SQ_SIZE                                     
             #if same sq is selected undo the move
                 if sq_sel == (column,row):   
                     sq_sel = ()
@@ -90,19 +48,23 @@ def main():
             #when the lenght of list if 2 that mean we have all moves     
                 if len(player_clicks) == 2:
                     #take the move that was made and check if it is legal.
-                    move = Move_Piece(player_clicks[0],player_clicks[1], b.board)      
+                    move = Move_Piece(player_clicks[0],player_clicks[1], gui.board.board)      
                     if move in valid_move:
-                        #when move is legal. Change the board. Set the move_check equal to true and set tuple and list to empty.
-                        b.move_on_board(move)
-                        print(b.piece_move())
+                        #when move is legal. Change the.board.ard. Set the move_check equal to true and set tuple and list to empty.
+                        gui.board.move_on.board.ard(move)
+                        print(gui.board.legal_move())
                         move_check = True
                         sq_sel = ()
                         player_clicks = []        
                     else:
                         player_clicks = [sq_sel]
 
+            elif event.type == pygame.KEYDOWN:                  
+                if event.type == pygame.K_LEFT:
+                    gui.board.undo_move()
+
         if move_check :
-            valid_move = b.legal_move() 
+            valid_move = gui.board.legal_move() 
             move_check = False
 
         w.draw_window()
